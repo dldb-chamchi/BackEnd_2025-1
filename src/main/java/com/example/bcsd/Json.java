64250944b6827b3aa939107b424d7a680559de80
dldb-chamchi <dladkfl49@gmail.com>
2025-05-05 01:52:48 +0900

    feat: json 요청시 반환

--- src/main/java/com/example/bcsd/Json.java
+++ src/main/java/com/example/bcsd/Json.java
@@ -1,9 +1,7 @@
 package com.example.bcsd;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
