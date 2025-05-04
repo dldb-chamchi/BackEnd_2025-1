92bf962599d70eb135584e0dd142aca9d8f61921
dldb-chamchi <dladkfl49@gmail.com>
2025-05-05 01:49:18 +0900

    introduce.html 완성

---
+++ src/main/java/com/example/bcsd/IntroduceNameController.java
@@ -0,0 +1,17 @@
+package com.example.bcsd;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IntroduceNameController {
+
+    @GetMapping(value = "/introduce2", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String introduce(@RequestParam(name = "name", required = true) String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
+    }
+}
---
+++ src/main/java/com/example/bcsd/Json.java
@@ -0,0 +1,33 @@
+package com.example.bcsd;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Json {
+
+    public class Introduction {
+        private int age;
+        private String name;
+
+
+        public Introduction(int age, String name) {
+            if(name==null){ throw new RuntimeException();}
+            this.age = age;
+            this.name = name;
+        }
+
+        public String getName() {return name;}
+
+        public int getAge() {return age;}
+    }
+
+    @GetMapping("/json")
+    @ResponseBody
+    public Introduction introduction() {
+        return new Introduction(26, "허준기");
+    }
+}
