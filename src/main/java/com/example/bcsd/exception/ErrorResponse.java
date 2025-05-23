a00b251c4d30e04eba7a0e720b239ba71b0967ac
dldb-chamchi <dladkfl49@gmail.com>
2025-05-23 10:34:17 +0900

    feat: ErrorResponse

--- src/main/java/com/example/bcsd/exception/ErrorResponse.java
+++ src/main/java/com/example/bcsd/exception/ErrorResponse.java
@@ -1,4 +1,14 @@
-package com.example.bcsd.Exception;
+package com.example.bcsd.exception;
 
 public class ErrorResponse {
+    private final int status;
+    private final String message;
+
+    public ErrorResponse(int status, String message) {
+        this.status  = status;
+        this.message = message;
+    }
+
+    public int getStatus()  { return status; }
+    public String getMessage(){ return message; }
 }
