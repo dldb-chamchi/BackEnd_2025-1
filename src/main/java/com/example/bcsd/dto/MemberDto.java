20faa12241c56c0052fe6242370f93de48eb6632
dldb-chamchi <dladkfl49@gmail.com>
2025-05-23 10:30:20 +0900

    feat: MemberDto

--- src/main/java/com/example/bcsd/dto/MemberDto.java
+++ src/main/java/com/example/bcsd/dto/MemberDto.java
@@ -1,4 +1,21 @@
-package com.example.bcsd.Dto;
+package com.example.bcsd.dto;
 
 public class MemberDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public MemberDto(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {return id;}
+    public String getName() {return name;}
+    public String getEmail() {return email;}
+    public String getPassword() {return password;}
+
 }
