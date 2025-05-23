a4448da67fc9afd35203ee2770cfa3e93f1aee9c
dldb-chamchi <dladkfl49@gmail.com>
2025-05-23 10:53:59 +0900

    fix: MemberRepository 구조 수정

--- src/main/java/com/example/bcsd/repository/BoardRepository.java
+++ src/main/java/com/example/bcsd/repository/BoardRepository.java
@@ -1,36 +1,62 @@
-package com.example.bcsd.Repository;
+package com.example.bcsd.repository;
 
-import com.example.bcsd.Model.Board;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.bcsd.model.Board;
+import com.example.bcsd.dto.ArticleDto;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.*;
 
 @Repository
 public class BoardRepository {
     private final JdbcTemplate jdbc;
 
-    @Autowired
     public BoardRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
 
-    private Board mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new Board(
+    public Optional<Board> findById(Long id) {
+        String sql = "SELECT id, name FROM board WHERE id = ?";
+        List<Board> list = jdbc.query(sql, boardRowMapper(), id);
+        return list.stream().findFirst();
+    }
+
+    public List<Board> findAll() {
+        String sql = "SELECT id, name FROM board";
+        return jdbc.query(sql, boardRowMapper());
+    }
+
+    public void save(Board b) {
+        if (b.getBoardId() == 0) {
+            jdbc.update("INSERT INTO board(name) VALUES(?)", b.getBoardTitle());
+        } else {
+            jdbc.update("UPDATE board SET name = ? WHERE id = ?", b.getBoardTitle(), b.getBoardId());
+        }
+    }
+
+    public void deleteById(Long id) {
+        jdbc.update("DELETE FROM board WHERE id = ?", id);
+    }
+
+    public String findBoardNameById(Long boardId) {
+        String sql = "SELECT name FROM board WHERE id = ?";
+        return jdbc.queryForObject(sql, String.class, boardId);
+    }
+
+    public List<ArticleDto> findArticlesByBoardId(Long boardId) {
+        String sql = "SELECT id, title FROM article WHERE board_id = ?";
+        return jdbc.query(sql, (rs, rn) ->
+                new ArticleDto(rs.getLong("id"),
+                        rs.getString("title")
+                ), boardId
+        );
+    }
+
+    private RowMapper<Board> boardRowMapper() {
+        return (rs, rowNum) -> new Board(
                 rs.getInt("id"),
                 rs.getString("name")
         );
     }
-
-    public Optional<Board> findById(int id) {
-        List<Board> list = jdbc.query(
-                "SELECT * FROM board WHERE id = ?",
-                this::mapRow, id
-        );
-        return list.stream().findFirst();
-    }
-
 }
