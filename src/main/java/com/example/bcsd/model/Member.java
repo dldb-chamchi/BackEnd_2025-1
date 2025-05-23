1a6b02babdc69ffd099fe90d6a210689eee51f2f
dldb-chamchi <dladkfl49@gmail.com>
2025-05-23 10:43:22 +0900

    feat: Member

--- src/main/java/com/example/bcsd/model/Member.java
+++ src/main/java/com/example/bcsd/model/Member.java
@@ -1,4 +1,4 @@
-package com.example.bcsd.Model;
+package com.example.bcsd.model;
 
 public class Member {
     private int memberId;
@@ -6,13 +6,23 @@ public class Member {
     private String email;
     private String password;
 
-    public Member(int id, String name){
-        if(name == null){ throw new RuntimeException();}
-        this.memberId = id;
+    public Member() {}
+
+    public Member(int memberId, String name, String email, String password) {
+        this.memberId = memberId;
         this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
     public int getMemberId() {return memberId;}
     public String getName() {return name;}
+    public String getEmail() {return email;}
+    public String getPassword() {return password;}
+
+    public void setMemberId(int memberId) {this.memberId = memberId;}
+    public void setName(String name) {this.name = name;}
+    public void setEmail(String email) {this.email = email;}
+    public void setPassword(String password) {this.password = password;}
 
 }
