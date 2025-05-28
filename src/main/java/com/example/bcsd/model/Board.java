5cf53fcfd470993a3fbdb3da2cac696dd5a4d6d0
dldb-chamchi <dladkfl49@gmail.com>
2025-05-28 15:08:26 +0900

    fix: JPA Board 추가

--- src/main/java/com/example/bcsd/model/Board.java
+++ src/main/java/com/example/bcsd/model/Board.java
@@ -1,30 +1,41 @@
 package com.example.bcsd.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "board")
 public class Board {
-    private int boardId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer boardId;
+
+    @Column(name = "name", nullable = false)
     private String boardTitle;
 
-    public Board(int boardId, String boardTitle) {
+    public Board() {}
+
+    public Board(Integer boardId, String boardTitle) {
         this.boardId = boardId;
         if(boardTitle == null) { throw new RuntimeException(); }
         this.boardTitle = boardTitle;
     }
 
-    public int getBoardId() {
+    public Integer getBoardId() {
         return boardId;
+    }
+
+    public void setBoardId(Integer boardId) {
+        this.boardId = boardId;
     }
 
     public String getBoardTitle() {
         return boardTitle;
     }
 
-    public void setBoardId(int boardId) {
-        this.boardId = boardId;
-    }
-
     public void setBoardTitle(String boardTitle) {
         if(boardTitle == null) { throw new RuntimeException(); }
         this.boardTitle = boardTitle;
     }
-
 }
