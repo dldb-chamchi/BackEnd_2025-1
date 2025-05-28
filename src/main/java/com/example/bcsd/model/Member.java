72040334ca01a01650ac5cc1892288eded798eda
dldb-chamchi <dladkfl49@gmail.com>
2025-05-28 15:09:29 +0900

    fix: JPA Member 추가

--- src/main/java/com/example/bcsd/model/Member.java
+++ src/main/java/com/example/bcsd/model/Member.java
@@ -1,50 +1,63 @@
 package com.example.bcsd.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "member")
 public class Member {
-    private int memberId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer memberId;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
     public Member() {}
 
-    public Member(int memberId, String name, String email, String password) {
+    public Member(Integer memberId, String name, String email, String password) {
         this.memberId = memberId;
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
-    public int getMemberId() {
+    public Integer getMemberId() {
         return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setMemberId(int memberId) {
-        this.memberId = memberId;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
-
 }
