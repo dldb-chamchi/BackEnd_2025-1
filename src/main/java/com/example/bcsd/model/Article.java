0717576a6319fce7fd00b6d4ce27884b95d1a44f
dldb-chamchi <dladkfl49@gmail.com>
2025-05-28 15:07:33 +0900

    fix: JPA Article 추가

--- src/main/java/com/example/bcsd/model/Article.java
+++ src/main/java/com/example/bcsd/model/Article.java
@@ -1,28 +1,39 @@
 package com.example.bcsd.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "article")
 public class Article {
-    private int articleId;
-    private String author;
-    private long boardId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer articleId;
+
+    @Column(name = "author_id", nullable = false)
+    private Integer authorId;
+
+    @Column(name = "board_id", nullable = false)
+    private Long boardId;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
+
+    @Column(name = "created_date", updatable = false)
     private String writeDateTime;
+
+    @Column(name = "modified_date")
     private String modifyDateTime;
 
     public Article() {}
 
-    public Article(int articleId, String author, int boardId, String title, String content, String writeDateTime) {
+    public Article(Integer articleId, Integer authorId, Long boardId, String title, String content, String writeDateTime, String modifyDateTime) {
         this.articleId = articleId;
-        this.author = author;
-        this.boardId = boardId;
-        this.title = title;
-        this.content = content;
-        this.writeDateTime = writeDateTime;
-    }
-
-    public Article(int articleId, String author, int boardId, String title, String content, String writeDateTime, String modifyDateTime) {
-        this.articleId = articleId;
-        this.author = author;
+        this.authorId = authorId;
         this.boardId = boardId;
         this.title = title;
         this.content = content;
@@ -30,47 +41,32 @@ public class Article {
         this.modifyDateTime = modifyDateTime;
     }
 
-    public int getArticleId() {
+    public Integer getArticleId() {
         return articleId;
     }
 
-    public String getAuthor() {
-        return author;
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
     }
 
-    public long getBoardId() {
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public Long getBoardId() {
         return boardId;
+    }
+
+    public void setBoardId(Long boardId) {
+        this.boardId = boardId;
     }
 
     public String getTitle() {
         return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getWriteDateTime() {
-        return writeDateTime;
-    }
-
-    public String getModifyDateTime() {
-        return modifyDateTime;
-    }
-
-    public void setId(int id) {
-        this.articleId = id;
-    }
-
-    public void setAuthor(String author) {
-        if (author == null || author.isEmpty()) {
-            throw new RuntimeException();
-        }
-        this.author = author;
-    }
-
-    public void setBoardId(long boardId) {
-        this.boardId = boardId;
     }
 
     public void setTitle(String title) {
@@ -80,6 +76,10 @@ public class Article {
         this.title = title;
     }
 
+    public String getContent() {
+        return content;
+    }
+
     public void setContent(String content) {
         if (content == null || content.isEmpty()) {
             throw new RuntimeException();
@@ -87,12 +87,19 @@ public class Article {
         this.content = content;
     }
 
+    public String getWriteDateTime() {
+        return writeDateTime;
+    }
+
     public void setWriteDateTime(String writeDateTime) {
         this.writeDateTime = writeDateTime;
+    }
+
+    public String getModifyDateTime() {
+        return modifyDateTime;
     }
 
     public void setModifyDateTime(String modifyDateTime) {
         this.modifyDateTime = modifyDateTime;
     }
-
 }
