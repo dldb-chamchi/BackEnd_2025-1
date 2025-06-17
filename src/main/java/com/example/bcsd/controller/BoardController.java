9220f1467db453062a0758e2b8065acf3c858c3c
dldb-chamchi <dladkfl49@gmail.com>
2025-06-17 16:16:37 +0900

    fix: Dto Controller 수정

--- src/main/java/com/example/bcsd/controller/BoardController.java
+++ src/main/java/com/example/bcsd/controller/BoardController.java
@@ -3,42 +3,36 @@ package com.example.bcsd.controller;
 import com.example.bcsd.requestDto.BoardRequestDto;
 import com.example.bcsd.responseDto.BoardPostsResponseDto;
 import com.example.bcsd.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/boards")
+@RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
 
-    public BoardController(BoardService boardService) {
-        this.boardService = boardService;
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<BoardPostsResponseDto> getBoard(@PathVariable("id") Long boardId) {
-        BoardPostsResponseDto dto = boardService.getPostsByBoardId(boardId);
-        return ResponseEntity.ok(dto);
-    }
-
     @PostMapping
     public ResponseEntity<Void> createBoard(@RequestBody BoardRequestDto req) {
-        boardService.createBoard(req.getBoardName());
+        boardService.createBoard(req);
         return ResponseEntity.status(201).build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardPostsResponseDto> getBoard(@PathVariable Long id) {
+        return ResponseEntity.ok(boardService.getPostsByBoardId(id));
+    }
+
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateBoard(
-            @PathVariable("id") Long boardId,
-            @RequestBody BoardRequestDto req
-    ) {
-        boardService.updateBoard(boardId, req.getBoardName());
+    public ResponseEntity<Void> updateBoard(@PathVariable("id") Long id, @RequestBody BoardRequestDto req) {
+        boardService.updateBoard(id, req);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBoard(@PathVariable("id") Long boardId) {
-        boardService.deleteBoard(boardId);
+    public ResponseEntity<Void> deleteBoard(@PathVariable Long id) {
+        boardService.deleteBoard(id);
         return ResponseEntity.noContent().build();
     }
 }
