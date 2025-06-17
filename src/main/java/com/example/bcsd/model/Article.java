12a0a9c35a211b56136ecf94aa7ea2f3a7df23a9
dldb-chamchi <dladkfl49@gmail.com>
2025-06-17 16:06:59 +0900

    fix: Lombok 설정

--- src/main/java/com/example/bcsd/model/Article.java
+++ src/main/java/com/example/bcsd/model/Article.java
@@ -1,21 +1,25 @@
 package com.example.bcsd.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "article")
+@Getter @Setter @NoArgsConstructor
 public class Article {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer articleId;
+    private Long articleId;
 
     @Column(name = "author_id", nullable = false)
-    private Integer authorId;
+    private Long authorId;
 
-    @Column(name = "board_id", nullable = false)
-    private Long boardId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
 
     @Column(nullable = false)
     private String title;
@@ -29,77 +33,10 @@ public class Article {
     @Column(name = "modified_date")
     private String modifyDateTime;
 
-    public Article() {}
-
-    public Article(Integer articleId, Integer authorId, Long boardId, String title, String content, String writeDateTime, String modifyDateTime) {
-        this.articleId = articleId;
+    public Article(Long authorId, Board board, String title, String content) {
         this.authorId = authorId;
-        this.boardId = boardId;
+        this.board = board;
         this.title = title;
         this.content = content;
-        this.writeDateTime = writeDateTime;
-        this.modifyDateTime = modifyDateTime;
-    }
-
-    public Integer getArticleId() {
-        return articleId;
-    }
-
-    public void setArticleId(Integer articleId) {
-        this.articleId = articleId;
-    }
-
-    public Integer getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(Integer authorId) {
-        this.authorId = authorId;
-    }
-
-    public Long getBoardId() {
-        return boardId;
-    }
-
-    public void setBoardId(Long boardId) {
-        this.boardId = boardId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        if (title == null || title.isEmpty()) {
-            throw new RuntimeException();
-        }
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        if (content == null || content.isEmpty()) {
-            throw new RuntimeException();
-        }
-        this.content = content;
-    }
-
-    public String getWriteDateTime() {
-        return writeDateTime;
-    }
-
-    public void setWriteDateTime(String writeDateTime) {
-        this.writeDateTime = writeDateTime;
-    }
-
-    public String getModifyDateTime() {
-        return modifyDateTime;
-    }
-
-    public void setModifyDateTime(String modifyDateTime) {
-        this.modifyDateTime = modifyDateTime;
     }
 }
