7951f0ec3c258a6725b5ef6f93ad6ceca216cd96
dldb-chamchi <dladkfl49@gmail.com>
2025-06-17 16:12:18 +0900

    fix: Lombok 설정

--- src/main/java/com/example/bcsd/model/Board.java
+++ src/main/java/com/example/bcsd/model/Board.java
@@ -1,41 +1,47 @@
 package com.example.bcsd.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "board")
+@Getter @Setter @NoArgsConstructor
 public class Board {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer boardId;
+    private Long boardId;
 
     @Column(name = "name", nullable = false)
     private String boardTitle;
 
-    public Board() {}
+    //Board, Article 1:N 연관관계
+    @OneToMany(
+            mappedBy = "board",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
 
-    public Board(Integer boardId, String boardTitle) {
-        this.boardId = boardId;
-        if(boardTitle == null) { throw new RuntimeException(); }
+    private List<Article> articles = new ArrayList<>();
+
+    public Board(String boardTitle) {
         this.boardTitle = boardTitle;
     }
 
-    public Integer getBoardId() {
-        return boardId;
+    //자식 추가
+    public void addArticle(Article a) {
+        articles.add(a);
+        a.setBoard(this);
     }
 
-    public void setBoardId(Integer boardId) {
-        this.boardId = boardId;
-    }
-
-    public String getBoardTitle() {
-        return boardTitle;
-    }
-
-    public void setBoardTitle(String boardTitle) {
-        if(boardTitle == null) { throw new RuntimeException(); }
-        this.boardTitle = boardTitle;
+    //자식 제거
+    public void removeArticle(Article a) {
+        articles.remove(a);
+        a.setBoard(null);
     }
 }
