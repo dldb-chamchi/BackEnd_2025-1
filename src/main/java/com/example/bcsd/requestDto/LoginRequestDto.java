87ca57d4bcd086387c3989c2504fe5bc2ddda4e9
dldb-chamchi <dladkfl49@gmail.com>
2025-06-30 13:07:14 +0900

    feat: LoginRequestDto 추가

--- src/main/java/com/example/bcsd/requestDto/LoginRequestDto.java
+++ src/main/java/com/example/bcsd/requestDto/LoginRequestDto.java
@@ -1,4 +1,11 @@
 package com.example.bcsd.requestDto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class LoginRequestDto {
+    private String email;
+    private String password;
 }
