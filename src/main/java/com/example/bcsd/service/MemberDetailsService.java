84c959b3b312926cc16fad4ad0f4af73134173a3
dldb-chamchi <dladkfl49@gmail.com>
2025-06-30 13:08:09 +0900

    feat: Member Login Service 추가

--- src/main/java/com/example/bcsd/service/MemberDetailsService.java
+++ src/main/java/com/example/bcsd/service/MemberDetailsService.java
@@ -1,4 +1,27 @@
 package com.example.bcsd.service;
 
-public class MemberDetailsService {
+import com.example.bcsd.model.Member;
+import com.example.bcsd.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member m = memberRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email));
+        return User.builder()
+                .username(m.getEmail())
+                .password(m.getPassword())
+                .roles("USER")
+                .build();
+    }
 }
