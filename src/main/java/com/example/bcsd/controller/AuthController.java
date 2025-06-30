8e149411ad107ceb97d897ae573d582db48f015f
dldb-chamchi <dladkfl49@gmail.com>
2025-06-30 13:06:10 +0900

    feat: AuthController 추가

--- src/main/java/com/example/bcsd/controller/AuthController.java
+++ src/main/java/com/example/bcsd/controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.example.bcsd.controller;
 
+import com.example.bcsd.requestDto.LoginRequestDto;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthenticationManager authManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody LoginRequestDto dto, HttpSession session) {
+        Authentication auth = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+        return ResponseEntity.ok().build();
+    }
 }
